3989720e75ae2adb3801814be1a1289b64851840
csillagbarnabas <47949207+csillagbarnabas@users.noreply.github.com>
2020-12-06 08:01:39 +0100

    Add files via upload

---
+++ task5/moldin2/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Felhasználó/Desktop/6.félév/számszim/5.feladat/MolekulaDinamika/moldin2
+# Build directory: C:/Users/Felhasználó/Desktop/6.félév/számszim/5.feladat/MolekulaDinamika/moldin2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
